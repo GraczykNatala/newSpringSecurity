40f434a28112a9dd2dde4d7f0afc41965b578b09
ngraczyk <natalia.graczyk@dxc.com>
2023-07-13 15:53:28 +0200

    Initial commit

---
+++ src/main/java/pl/gr/newspringsecurity/user/Role.java
@@ -0,0 +1,8 @@
+package pl.gr.newspringsecurity.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/pl/gr/newspringsecurity/NewSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gr.newspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
